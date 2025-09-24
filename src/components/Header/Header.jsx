22174d634708b9ac501ba90380d605f1f5cc9bcc
John Angel <jhonlm1010@hotmail.com>
2025-09-23 19:16:00 -0500

    fix: correct casing of Header.css import

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './header.css';  
+import './Header.css';  
 import logo from '../../assets/logo-anto-store.jpeg';
 import { Link } from 'react-router-dom';
 import { useCart } from '../../context/CartContext';
